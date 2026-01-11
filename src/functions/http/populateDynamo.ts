7634605c65f055ff90a50c1d7829dc7ca15368e0
IanFsciety <iandelas123@outlook.com>
2026-01-11 19:12:30 -0400

    fix total

--- src/functions/http/populateDynamo.ts
+++ src/functions/http/populateDynamo.ts
@@ -1,14 +1,16 @@
-import { PutCommand } from '@aws-sdk/lib-dynamodb';
+
 import { faker } from '@faker-js/faker';
 import { randomUUID } from 'node:crypto';
+
+import { PutCommand } from '@aws-sdk/lib-dynamodb';
 import { dynamoClient } from '../../clients/dynamoClient';
 import { env } from '../../config/env';
 import { response } from '../../utils/response';
 
 export async function handler() {
-  const total = 2;
+  const total = 500;
 
-  const responses =  await Promise.allSettled(
+  const responses = await Promise.allSettled(
     Array.from({ length: total }, async () => {
       const command = new PutCommand({
         TableName: env.DYNAMO_LEADS_TABLE,
@@ -24,9 +26,9 @@ export async function handler() {
     })
   );
 
-  const successTotal = responses.filter(result => result.status === 'fulfilled').length;
+  const totalCreatedLeads = responses.filter(
+    result => result.status === 'fulfilled',
+  ).length;
 
-  return response(201, {
-    totalLeadsCreated: successTotal,
-  });
+  return response(201, { totalCreatedLeads });
 }
